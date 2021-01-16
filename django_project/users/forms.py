a70b8c83084fa3cf8d8f6b33fe307be7e424097f
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-01-16 06:17:12 +0530

    Added Login and logout pages

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -10,8 +12,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(
+                request, f'Your account has been created! You can now Log In!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
